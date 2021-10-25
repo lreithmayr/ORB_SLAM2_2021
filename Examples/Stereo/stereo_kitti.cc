2ca7280b3d3198d48e4195560c1274211b6d39cc
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-10-25 15:48:50 +0200

    Cosmetic change

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -81,11 +81,7 @@ int main(int argc, char **argv)
         // Pass the images to the SLAM system
         SLAM.TrackStereo(imLeft,imRight,tframe);
 
-// #ifdef COMPILEDWITHC11
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-// #else
-//        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-// #endif
 
         double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
 
