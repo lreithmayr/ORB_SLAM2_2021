595b6e97f140d296220cfa729954293ce100134b
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-10-25 13:01:48 +0200

    Changes to build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -DCMAKE_BUILD_TYPE=Release >> DBoW2_output_file.txt 2>&1
+make -j8
 
 cd ../../g2o
 
@@ -12,8 +12,8 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -DCMAKE_BUILD_TYPE=Release >> g2o_output_file.txt 2>&1
+make -j8
 
 cd ../../../
 
@@ -27,5 +27,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+cmake .. -DCMAKE_BUILD_TYPE=Release >> OS2_output_file.txt 2>&1
+make -j8
