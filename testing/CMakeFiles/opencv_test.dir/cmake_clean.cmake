323796f62c88ceb8c93145644a477f73d84e1705
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-10-26 17:27:28 +0200

    IPWebcam input working; calibration needed.

---
+++ testing/CMakeFiles/opencv_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_test.dir/opencv_test.cpp.o"
+  "opencv_test"
+  "opencv_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
