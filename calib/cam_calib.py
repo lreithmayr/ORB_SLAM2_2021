8ed0a9748ab8330d6f1bd282e898fe9c1d3c63ae
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-10-27 13:43:13 +0200

    Added camera calibration script

---
+++ calib/cam_calib.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import cv2 as cv
+import numpy as np
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
+images = glob.glob('./images/*.jpg')
+for fname in images:
+    if fname is None:
+        break
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+
+cv.destroyAllWindows()
+
+# h,w = img.shape[:2]
+
+"""
+Performing camera calibration by 
+passing the value of known 3D points (objpoints)
+and corresponding pixel coordinates of the 
+detected corners (imgpoints)
+"""
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+print("Camera matrix : \n")
+print(mtx)
+print("dist : \n")
+print(dist)
+print("rvecs : \n")
+print(rvecs)
+print("tvecs : \n")
+print(tvecs)
