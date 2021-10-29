b1b420dc3c09aaeef56c167394a1974f7ace4d5d
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-10-29 10:36:53 +0200

    Camera calibration working.

--- calib/cam_calib.py
+++ calib/cam_calib.py
@@ -4,18 +4,18 @@ import cv2 as cv
 import numpy as np
 import glob
 
-# termination criteria
+# Termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6 * 7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+# Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6 * 8, 3), np.float32)
+objp[:, :2] = np.mgrid[0:6, 0:8].T.reshape(-1, 2)
 
-# Arrays to store object points and image points from all the images_ocv.
+# Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane
 
-images = glob.glob('images_ocv/*.jpg')
+images = glob.glob('images/*.jpg')
 for fname in images:
     if fname is None:
         break
@@ -24,8 +24,8 @@ for fname in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
-    cv.drawChessboardCorners(gray, (7, 6), corners, ret)
+    ret, corners = cv.findChessboardCorners(gray, (6, 8), None)
+    cv.drawChessboardCorners(gray, (6, 8), corners, ret)
 
     # If found, add object points, image points (after refining them)
     if ret:
@@ -33,9 +33,13 @@ for fname in images:
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv.drawChessboardCorners(img, (6, 8), corners2, ret)
+        cv.imshow("foundCorners", gray)
         cv.imshow('img', img)
         cv.waitKey(500)
+        print("Corners found.")
+    else:
+        print("No corners found.")
 
 cv.destroyAllWindows()
 
