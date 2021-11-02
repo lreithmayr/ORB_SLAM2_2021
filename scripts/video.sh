837148117bf03cde484b523f9b8738a26c3a19ff
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-11-02 14:09:22 +0100

    Removed unused includes.

--- scripts/video.sh
+++ scripts/video.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-../Examples/Monocular/mono_video  ../Vocabulary/ORBvoc.txt ../calib/PhoneLR.yaml /home/lorenz/Videos/VID_20211102_101603_resized.mp4
+../Examples/Monocular/mono_video  ../Vocabulary/ORBvoc.txt ../calib/PhoneLR.yaml /home/lorenz/Videos/VID_20211102_134423_res.mp4
+
