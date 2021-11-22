e9c3785aaeb2c27402bb456fb32e64b675d2dc97
psychedelic-tortilla <lorenz.reithmayr@t-online.de>
2021-11-22 16:45:10 +0100

    Comments

--- gridmapper/include/MapProcessor.h
+++ gridmapper/include/MapProcessor.h
@@ -12,10 +12,19 @@ namespace ORB_SLAM2
 class MapProcessor
 {
 public:
+    // Constructor initializes binary map, and ORB vocabulary for relocalization
     explicit MapProcessor(const string &filename);
+
+    // Generates occupancy grid map from KFs and Map Points. Bugged.
     void SaveGridMapKITTI(const string &filename);
+
+    // Saves KF trajectory in txt file. Translation t and Quaternions.
     void SaveTrajectoryKITTI(const string &filename);
+
+    // Extracts map points and the timestamps at which they are observed in different KFs
     void SavePointCloud(const string &filename);
+
+    // Opens the generated grid map as a pgm file.
     static void OpenMap(const string &filename);
 
 private:
