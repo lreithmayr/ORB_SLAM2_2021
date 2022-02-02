0bc023189cce8b171cc7518e888e857ef247f34e
Lorenz Reithmayr <lorenz.reithmayr@t-online.de>
2022-02-02 15:52:31 +0100

    Accurate timestamps

--- Examples/Stereo/arducam_stereo.cpp
+++ Examples/Stereo/arducam_stereo.cpp
@@ -40,8 +40,46 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // Read rectification parameters
+    cv::FileStorage fsSettings(argv[2], cv::FileStorage::READ);
+    if(!fsSettings.isOpened())
+    {
+        cerr << "ERROR: Wrong path to settings" << endl;
+        return -1;
+    }
+
+    cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
+    fsSettings["LEFT.K"] >> K_l;
+    fsSettings["RIGHT.K"] >> K_r;
+
+    fsSettings["LEFT.P"] >> P_l;
+    fsSettings["RIGHT.P"] >> P_r;
+
+    fsSettings["LEFT.R"] >> R_l;
+    fsSettings["RIGHT.R"] >> R_r;
+
+    fsSettings["LEFT.D"] >> D_l;
+    fsSettings["RIGHT.D"] >> D_r;
+
+    int rows_l = fsSettings["LEFT.height"];
+    int cols_l = fsSettings["LEFT.width"];
+    int rows_r = fsSettings["RIGHT.height"];
+    int cols_r = fsSettings["RIGHT.width"];
+
+    if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
+            rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
+    {
+        cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
+        return -1;
+    }
+
+    cv::Mat M1l,M2l,M1r,M2r;
+    cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,M1l,M2l);
+    cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,M1r,M2r);
+
+
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::STEREO,true, true);
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::STEREO, true, false);
 
     CAMERA_INSTANCE camera_instance;
 
@@ -69,7 +107,10 @@ int main(int argc, char **argv)
          LOG("Notice:You can use the list_format sample program to see the resolution and control supported by the camera.");
     }
     
+    cv::Mat imLeftRect;
+    cv::Mat imRightRect;
 
+    double tframe = 0;
     while(true)
     {
         cv::Mat* image = get_image(camera_instance, width, height);
@@ -79,10 +120,18 @@ int main(int argc, char **argv)
 	    cv::Mat imLeft = img(cv::Rect(0, 0, (width/2), height));
 	    cv::Mat imRight = img(cv::Rect((width/2), 0, (width/2), height)); 
 
-        // FIXME: Get actual time between frames.
-        double tframe = (double) 21.00053;
+        cv::remap(imLeft,imLeftRect,M1l,M2l,cv::INTER_LINEAR);
+        cv::remap(imRight,imRightRect,M1r,M2r,cv::INTER_LINEAR);
+
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
         SLAM.TrackStereo(imLeft, imRight, tframe);
+
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+
+        double tracking_time = std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+        tframe = tframe + tracking_time;
 
 	    delete image;
     
