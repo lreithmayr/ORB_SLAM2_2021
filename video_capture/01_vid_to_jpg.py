fb6ca843d84cd8a9029f3873ac50d979f006c214
Lorenz Reithmayr <lorenz.reithmayr@t-online.de>
2022-02-10 14:14:42 +0100

    Frame saver runs at about 10 fps now.

--- video_capture/01_vid_to_jpg.py
+++ video_capture/01_vid_to_jpg.py
@@ -1,3 +1,11 @@
+"""
+
+This script receives an input feed from the Arducam stereo camera and saves a frame at a rate of 10 frames per second to a folder.
+
+The name of the folder can be chosen by altering the variable "sequence_name" at the beginning of the program.
+
+"""
+
 import os
 import time
 from datetime import datetime
@@ -6,8 +14,8 @@ import json
 import cv2
 import numpy as np
 
-sequence_name = "imse_werkstatt"
-os.makedirs(sequence_name, exist_ok=True)
+sequence_name = "fps_test"
+os.makedirs("./videos/" + sequence_name, exist_ok=True)
 
 def align_down(size, align):
     return (size & ~((align)-1))
@@ -44,28 +52,49 @@ img_width = cam_width
 img_height = cam_height 
 print ("Scaled image resolution: "+str(img_width)+" x "+str(img_height))
 
+framecount = 0
 counter = 10000
 ts = []
 timestamp = 0
 print ("Starting video.")
 while True:
-    ts.append(timestamp)
-    print(counter, timestamp)
     t1 = datetime.now()
+    framecount = framecount + 1
+    ts.append(timestamp)
+    print("Frame Number | Timestamp: %s" % framecount, timestamp)
+
     frame = get_frame(camera)
-    filename = './' + sequence_name + '/' + str(counter) + '.jpg'
+    filename = './videos/' + sequence_name + '/' + str(counter) + '.jpg'
     cv2.imwrite(filename, frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
+
+    counter = counter + 1
+
+    # fps = framecount / timestamp
+    # print("FPS: %s" % int(fps))
+
+
+    # key = (cv2.waitKey(1) & 0xFF)
+    # if key == 27:
+    #     break
+
+    # delta_t = (t2 - t1).total_seconds()
+
+    # if framecount > 6:
+    #     add_time = 0.1 - delta_t 
+    #     time.sleep(add_time)
+
+
+    cv2.imshow("Window", frame)
+    if cv2.waitKey(60) == 27:
+        break;
+
     t2 = datetime.now()
     delta_t = (t2 - t1).total_seconds()
-    counter = counter + 1
-    timestamp = timestamp + delta_t
-    cv2.imshow("Window", frame)
-    key = (cv2.waitKey(1) & 0xFF)
-    if key == 27:
-        break
+    timestamp = timestamp + delta_t 
+
 print ("Video finished")
 
-f = open('./' + sequence_name + '/' + 'times.txt', "w")
+f = open('./videos/' + sequence_name + '/' + 'times.txt', "w")
 for time in ts:
     f.write(str(time) + "\n")
 f.close()
